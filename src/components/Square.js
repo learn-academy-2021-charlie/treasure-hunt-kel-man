461f00412348e605c2662c2a986c4827c8748162
kel-man <kyyafuso@gmail.com>
2021-07-26 14:12:34 -0700

    clicking on a square alerts the index of that square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+  handleClick = () => {
+    this.props.handleGameplay(this.props.index)
+  }
   render(){
     return(
       <>
-        <div id='square'>
+        <div id='square' onClick={() => this.handleClick(this.props.index)}>
           {this.props.value}
         </div>
       </>
