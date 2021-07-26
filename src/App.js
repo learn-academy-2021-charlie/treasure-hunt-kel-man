08f28899638c05a27f8fbb8911d4fca18fe7b26e
kel-man <kyyafuso@gmail.com>
2021-07-26 14:29:50 -0700

    clicking a box turns the question mark to a tree

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component{
   }
 
   handleGameplay = (index) => {
-    alert(index)
+    const { board } = this.state
+    board[index] = '🌴'
+    this.setState({board: board})
   }
 
   render(){
