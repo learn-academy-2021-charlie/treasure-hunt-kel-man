05ae3b44517fa250a6b6cc1430ffc104be5aaa55
kel-man <kyyafuso@gmail.com>
2021-07-26 13:58:45 -0700

    adds gameboard with 9 squares and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ['?', '?', '?', '?', '?', '?', '?', '?', '?']
     }
   }
 
@@ -13,6 +14,11 @@ class App extends Component{
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div id='gameboard'>
+          {this.state.board.map((value, index) => {
+             return <Square value={value} key={index}/>
+          })}
+        </div>
       </>
     )
   }
